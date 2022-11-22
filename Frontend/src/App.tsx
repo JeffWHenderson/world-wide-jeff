c8835319b7f7065e29f9b6819e6df68af119168f
jeffrey Henderson <jeffreyhenderson@jeffreys-Air.attlocal.net>
2022-11-21 20:55:27 -0500

    [jeff] initial commit!!!

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome To The World Wide Jeff
+      </header>
+    </div>
+  );
+}
+
+export default App;
