498cf15b2177f6fff554ee9249edb13498a2dd7e
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-05-12 17:14:56 -0400

    add some date stuff to the model... prepping for a backend yo

--- src/screens/Events.ts
+++ src/screens/Events.ts
@@ -2,7 +2,7 @@ type event = {
     id: string;
     name: string;
     details: string;
-    startDate?: string; // use date time stamp. no offset?
+    startDate: "tbd" | "cancelled" | string; // use date time stamp. no offset?
     // recurring?: boolean;
     // plannedByJeff?: boolean;
     // local?: boolean;
@@ -12,88 +12,105 @@ type event = {
 
 export const events: event[] = [
     {
-        id: "some-id--2",
-        name: "Hike - North Georgia",
-        details: "Lets look at trees and maybe a waterfall"
-    },
-    {
-        id: "some-id--3",
-        name: "Kayak & Paddleboard",
-        details: "day on the water and then Halal after"
-    },
-    {
-        id: "some-id--4",
-        name: "Hot air Balloon",
-        details: "I'm not really even sure that  this is a thing"
-    },
-    {
-        id: "some-id",
-        name: "zombie walk",
-        details: "A beltline bar crawl with some sick zombie makeup"
-    },
-    {
-        id: "some-id-2",
-        name: "pool party",
-        details: "Pool party at the condo"
-    },
-    {
-        id: "some-id-3",
-        name: "Wall Ride",
-        details: "twice a month group bike ride"
-    },
-    {
-        id: "some-id-4",
-        name: "Piedmont Heights Pickleball League",
-        details: "a very poorly managed (by me) pickleball league"
-    },
-    {
-        id: "some-id-6",
-        name: "Outdoor Movie Night: The Room",
-        details: "Lets watch a movie by the pool"
-    },
-    {
-        id: "some-id-7",
-        name: "Texas Hold em Poker",
-        details: "Bluffing encouraged"
-    },
-    {
         id: "some-id-8",
         name: "Virginia Highlands Porchfest",
-        details: "I'm not planning this but its a whole thing we should get a group together to do"
+        details: "I'm not planning this but its a whole thing we should get a group together to do",
+        startDate: "2025-5-17"
     },
     {
         id: "some-id-9",
         name: "Lantern Festival",
-        details: "Lets meet before and make some lanterns. I'm not planning the actual event but its a whole thing we should get a group together to do"
+        details: "Lets meet before and make some lanterns. I'm not planning the actual event but its a whole thing we should get a group together to do",
+        startDate: "cancelled"
     },
     {
         id: "some-id-10",
         name: "Dragon Con",
-        details: "Get your tickets now"
-    },
-    {
-        id: "some-id-11",
-        name: "Karoake night",
-        details: "Go big or go home"
+        details: "Get your tickets now",
+        startDate: "2025-08-28"
     },
     {
         id: "some-id-12",
         name: "Rennaisance Festival",
-        details: "huzzah?"
+        details: "huzzah?",
+        startDate: "2025-5-18"
+    },
+    {
+        id: "some-id--2",
+        name: "Hike - North Georgia",
+        details: "Lets look at trees and maybe a waterfall",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id--3",
+        name: "Kayak & Paddleboard",
+        details: "day on the water and then Halal after",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id--4",
+        name: "Hot air Balloon",
+        details: "I'm not really even sure that  this is a thing",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id",
+        name: "zombie walk",
+        details: "A beltline bar crawl with some sick zombie makeup",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-2",
+        name: "pool party",
+        details: "Pool party at the condo",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-3",
+        name: "Wall Ride",
+        details: "twice a month group bike ride",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-4",
+        name: "Piedmont Heights Pickleball League",
+        details: "a very poorly managed (by me) pickleball league",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-6",
+        name: "Outdoor Movie Night: The Room",
+        details: "Lets watch a movie by the pool",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-7",
+        name: "Texas Hold em Poker",
+        details: "Bluffing encouraged",
+        startDate: "tbd"
+    },
+    {
+        id: "some-id-11",
+        name: "Karoake night",
+        details: "Go big or go home",
+        startDate: "tbd"
     },
     {
         id: "some-id-14",
         name: "Atlanta streets alive",
-        details: "dates"
+        details: "dates",
+        startDate: "tbd"
     },
     {
         id: "some-id-15",
         name: "Hot Pot",
-        details: "Just need to remember I like doing these"
+        details: "Just need to remember I like doing these",
+        startDate: "tbd"
     },
     {
         id: "some-id-16",
         name: "slack line",
-        details: "invite people to do a slackline but also don't let people think i'm one of those slackline guys in any way"
+        details: "invite people to do a slackline but also don't let people think i'm one of those slackline guys in any way",
+        startDate: "tbd"
     }
 ]
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,18 @@
 import { events } from "./Events"
 
 const Home = () => {
+    const displayDate = (date: "tbd" | "cancelled" | string) => {
+        if (date === 'cancelled') {
+            return <span style={{ color: 'black', backgroundColor: "red", padding: "4px", borderRadius: "10px" }}>{date}</span>
+        }
+
+        if (date === 'tbd') {
+            return <span style={{ color: 'black', backgroundColor: "yellow", padding: "4px", borderRadius: "10px" }}>{date}</span>
+        }
+
+        return <span style={{ color: 'black', backgroundColor: "white", padding: "4px", borderRadius: "10px" }}>{date.split("2025-")[1]}</span>
+    }
+
     return (
         <>
             {/* EVENT CARD START */}
@@ -8,7 +20,10 @@ const Home = () => {
             {
                 events.map(item => (
                     <div style={{ backgroundColor: "grey", border: "1px solid grey", margin: "2px", borderRadius: "10px", padding: "0px 4px 0px 8px" }} key={item.id} onClick={() => console.log("clicked " + item.id)}>
-                        <h4> {item.name}</h4 >
+                        <div>
+                            <h4> {displayDate(item.startDate)} {item.name}</h4 >
+                        </div>
+
                         <p>{item.details}</p>
                     </div>
                 ))
