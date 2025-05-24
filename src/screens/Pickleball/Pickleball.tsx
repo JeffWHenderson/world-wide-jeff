576aea35971cde62024de6dd55aa8d7342f08a36
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-05-23 21:40:53 -0400

    Add some details to the pickleball section

--- src/screens/Pickleball/Pickleball.tsx
+++ src/screens/Pickleball/Pickleball.tsx
@@ -1,7 +1,18 @@
 import UnderConstruction from "../Core/UnderConstruction";
 
 const Pickleball = () => {
-    return <UnderConstruction name="Pickleball League" />
+    return <>
+        <UnderConstruction name="Pickleball League" />
+        <p>
+            Details to come...
+        </p>
+        <p>
+            The idea is to host a few meetup days to get people at the condo's tennis court.  If we start collecting enough people I'll start organizing with sign ups and maybe some actual effort into this webpage.
+        </p>
+        <p>
+            It could also be tennis, I just think that would involve more skill matching, and I don't think I'm smart enought for that.
+        </p>
+    </>
 };
 
 export default Pickleball;
