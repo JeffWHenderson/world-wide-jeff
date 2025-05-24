24ecb564009056025d8167b859a503ff93004046
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-05-23 21:06:00 -0400

    still bad code but I can go to an event page.. should help me figure out all the stuff i need there like photos and links, whatever.  I'll get to making proper routes and styles later.

--- src/screens/Events/Events.tsx
+++ src/screens/Events/Events.tsx
@@ -1,6 +1,9 @@
-import { hardCodedEvents } from "./EventTypes"
+import { useState } from "react"
+import { EventDetails, hardCodedEvents } from "./EventTypes"
 
 const Events = () => {
+    const [selectedEvent, setSelectedEvent] = useState<EventDetails | null>(null)
+    
     const displayDate = (date: "tbd" | Date) => {
         if (date === 'tbd') {
             return <span style={{ color: 'black', backgroundColor: "yellow", padding: "4px", borderRadius: "10px" }}>{date}</span>
@@ -8,6 +11,30 @@ const Events = () => {
 
         // TODO: Better Date Formatting
         return <span style={{ color: 'black', backgroundColor: "white", padding: "4px", borderRadius: "10px" }}>{date.toString().split(" 2025")[0]}</span>
+    }
+
+    const handleOpenEventPage = (item: EventDetails) => {
+        setSelectedEvent(item);
+    }
+
+    if (selectedEvent) {
+        return (
+            <>
+                <h1>{selectedEvent.name}</h1>
+                <p>{selectedEvent.startDate.toString()}</p>
+                <p>{selectedEvent.details}</p>
+                <p>{selectedEvent.status}</p>
+                {selectedEvent?.links?.length && selectedEvent?.links?.length > 0 &&
+                    <>
+                        <h3>Links</h3>
+                        <a href={selectedEvent.links[0].link}> {selectedEvent.links[0].linkDescription}</a>
+
+                    </>
+                }
+                <br /><br /><br />
+                <button onClick={() => setSelectedEvent(null)}>GO BACK!!!</button>
+            </>
+        )
     }
 
     return (
@@ -18,7 +45,7 @@ const Events = () => {
                 hardCodedEvents.map(item => (
                     // TODO: this is just a hack while I don't have internet to figure out the best way to handle dates
                     !item.startDate.toString().includes("May") &&
-                    <div style={{ backgroundColor: "grey", border: "1px solid grey", margin: "2px", borderRadius: "10px", padding: "0px 4px 0px 8px" }} key={item.id} onClick={() => console.log("clicked " + item.id)}>
+                    <div style={{ backgroundColor: "grey", border: "1px solid grey", margin: "2px", borderRadius: "10px", padding: "0px 4px 0px 8px" }} key={item.id} onClick={() => handleOpenEventPage(item)}>
                         <div>
                             <h4> {displayDate(item.startDate)} {item.name}</h4 >
                         </div>
@@ -34,7 +61,7 @@ const Events = () => {
                 hardCodedEvents.map(item => (
                     // TODO: this is just a hack while I don't have internet to figure out the best way to handle dates
                     item.startDate.toString().includes("May") &&
-                    <div style={{ backgroundColor: "grey", border: "1px solid grey", margin: "2px", borderRadius: "10px", padding: "0px 4px 0px 8px" }} key={item.id} onClick={() => console.log("clicked " + item.id)}>
+                    <div style={{ backgroundColor: "grey", border: "1px solid grey", margin: "2px", borderRadius: "10px", padding: "0px 4px 0px 8px" }} key={item.id} onClick={() => handleOpenEventPage(item)}>
                         <div>
                             <h4> {displayDate(item.startDate)} {item.name}</h4 >
                         </div>
