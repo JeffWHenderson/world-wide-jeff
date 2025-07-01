48b9fb7f97076e5ea90da63fc07a2c10f53b943b
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-06-30 22:17:31 -0400

    add speech structure

--- src/screens/Language/FlashCards.tsx
+++ src/screens/Language/FlashCards.tsx
@@ -4,34 +4,55 @@ import { translations } from "./translations";
 const FlashCards = () => {
     const [displayLanguage, setDisplayLanguage] = useState("Spanish")
     const [displayCharacters, setDisplayCharacters] = useState<string | null>(null)
+    const [voiceIndex, setVoiceIndex] = useState(8)
+    let voices = speechSynthesis.getVoices();
+
+
+    function speak() {
+        const message = new SpeechSynthesisUtterance();
+        message.text = translations[0][displayLanguage];
+        message.volume = 1; // Volume range = 0 - 1
+        message.rate = 1.1; // Speed of the text read , default 1
+        message.voice = voices[voiceIndex]; // change voice
+        message.lang = 'en-GB'; // Language, default 'en-US'
+        window.speechSynthesis.speak(message);
+        console.log(voices)
+    }
 
     const handleChangeDisplayLang = () => {
         if (displayLanguage == "Spanish") {
-            setDisplayCharacters("Chinese")
-            setDisplayLanguage("ChinesePinyin")
+            setVoiceIndex(25)
+            setDisplayLanguage("Chinese")
+            setDisplayCharacters("ChinesePinyin")
         }
-        if (displayLanguage == "ChinesePinyin") {
+        if (displayLanguage == "Chinese") {
+            setVoiceIndex(18)
             setDisplayCharacters("Romanji")
             setDisplayLanguage("Japanese")
         }
         if (displayLanguage == "Japanese") {
+            setVoiceIndex(23)
             setDisplayCharacters("Arabic-English")
             setDisplayLanguage("arabic")
         }
         if (displayLanguage == "arabic") {
+            setVoiceIndex(0)
             setDisplayCharacters(null)
             setDisplayLanguage("English")
         }
         if (displayLanguage == "English") {
+            setVoiceIndex(8)
             setDisplayLanguage("Spanish")
         }
     }
 
     return <>
-        <div onClick={handleChangeDisplayLang} style={{ margin: '8px' }}>
+        <button onClick={() => speak()}>speak</button>
+        <button onClick={handleChangeDisplayLang}>changeLang</button>
+        <div>
             {
                 translations.map(phrase => (
-                    <div style={{ height: "3em" }}>
+                    <div key={phrase["English"]} style={{ height: "3em" }}>
                         <div style={{ height: "1.5em" }}>{phrase[displayLanguage]}</div>
                         {displayCharacters ? <div style={{ color: 'red', height: "1.5em" }}>{phrase[displayCharacters]}</div> : null}
                     </div>
