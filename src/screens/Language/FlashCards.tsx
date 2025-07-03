bb5f242ccb3ad65a53f5f3ab0d4a46b5cd1bea62
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-07-03 14:00:59 -0400

    just display all the languages at the same time

--- src/screens/Language/FlashCards.tsx
+++ src/screens/Language/FlashCards.tsx
@@ -2,66 +2,80 @@ import { useState } from "react";
 import { translations } from "./translations";
 
 const FlashCards = () => {
-    const [displayLanguage, setDisplayLanguage] = useState("Spanish")
-    const [displayCharacters, setDisplayCharacters] = useState<string | null>(null)
-    const [voiceIndex, setVoiceIndex] = useState(8)
+    const [languages] = useState(["Spanish", "Chinese", "Japanese", "arabic"])
     const [cardNumber, setCardNumber] = useState(0)
-    const [showEnglish, setShowEnglish] = useState(false)
+    const [showEnglish] = useState(true)
+
     let voices = speechSynthesis.getVoices();
 
 
-    function speak(phrase: string) {
+
+    function speak(phrase: string, lang: string) {
         const message = new SpeechSynthesisUtterance();
         message.text = phrase;
-        message.volume = 1; // Volume range = 0 - 1
-        message.rate = 1.1; // Speed of the text read , default 1
-        message.voice = voices[voiceIndex]; // change voice
-        message.lang = 'en-GB'; // Language, default 'en-US'
-        window.speechSynthesis.speak(message);
-        console.log(voices)
-    }
 
-    const handleChangeDisplayLang = () => {
-        if (displayLanguage == "Spanish") {
-            setVoiceIndex(25)
-            setDisplayLanguage("Chinese")
-            setDisplayCharacters("ChinesePinyin")
+        if (lang == "Spanish") {
+            message.voice = voices[8]
         }
-        if (displayLanguage == "Chinese") {
-            setVoiceIndex(18)
-            setDisplayCharacters("Romanji")
-            setDisplayLanguage("Japanese")
+        if (lang == "Chinese") {
+            message.voice = voices[25]
         }
-        if (displayLanguage == "Japanese") {
-            setVoiceIndex(23)
-            setDisplayCharacters("Arabic-English")
-            setDisplayLanguage("arabic")
+        if (lang == "Japanese") {
+            message.voice = voices[18]
         }
-        if (displayLanguage == "arabic") {
-            setVoiceIndex(0)
-            setDisplayCharacters(null)
-            setDisplayLanguage("English")
+        if (lang == "arabic") {
+            message.voice = voices[23]
         }
-        if (displayLanguage == "English") {
-            setVoiceIndex(8)
-            setDisplayLanguage("Spanish")
+        if (lang == "English") {
+            message.voice = voices[0]
         }
+
+        window.speechSynthesis.speak(message);
+        // message.volume = 1; // Volume range = 0 - 1
+        // message.rate = 1.1; // Speed of the text read , default 1
+        // message.lang = 'en-GB'; // Language, default 'en-US'
     }
 
     const nextCard = () => {
         setCardNumber(cardNumber + 1)
-        speak(translations[cardNumber + 1][displayLanguage])
+
     }
 
     return <>
-        <button onClick={handleChangeDisplayLang}>changeLang</button>
+        {/* <div style={{ marginBottom: "5px" }}>
+            <label for="english">
+                <input type="checkbox" id="english"></input>
+                English
+            </label>
+            <label for="arabic">
+                <input type="checkbox" id="arabic"></input>
+                Arabic
+            </label>
+            <label for="chinese">
+                <input type="checkbox" id="chinese"></input>
+                Chinese
+            </label>
+            <label for="japanese">
+                <input type="checkbox" id="japanese"></input>
+                Japanese
+            </label>
+            <label for="spanish">
+                <input type="checkbox" id="spanish"></input>
+                Spanish
+            </label>
+        </div> */}
+
+        {showEnglish ? <div style={{}}>{translations[cardNumber]["English"]}</div> : null}
+
+        {languages.map((lang) => (
+            <div style={{ border: "1px solid grey", borderRadius: "4px", margin: "3px" }} onClick={() => speak(translations[cardNumber][lang], lang)}>
+                {lang === "arabic" ? <div style={{ color: 'red', height: "1.5em" }}>{translations[cardNumber]["Arabic-English"]}</div> : null}
+                {lang === "Japanese" ? <div style={{ color: 'red', height: "1.5em" }}>{translations[cardNumber]["Romanji"]}</div> : null}
+                {lang === "Chinese" ? <div style={{ color: 'red', height: "1.5em" }}>{translations[cardNumber]["ChinesePinyin"]}</div> : null}
+                {translations[cardNumber][lang]}
+            </div>
+        ))}
         <button onClick={() => nextCard()}>next</button>
-        <button onClick={() => setShowEnglish(!showEnglish)}>show English</button>
-        <div onClick={() => speak(translations[cardNumber][displayLanguage])}>
-            {displayCharacters ? <div style={{ color: 'red', height: "1.5em" }}>{translations[cardNumber][displayCharacters]}</div> : null}
-            {translations[cardNumber][displayLanguage]}
-            {showEnglish ? <p>{translations[cardNumber]["English"]}</p> : null}
-        </div>
     </>
 }
 
