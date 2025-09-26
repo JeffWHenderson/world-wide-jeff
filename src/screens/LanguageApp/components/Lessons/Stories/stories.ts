263ee521619040848f6e6c403d4e175d79b0c865
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-09-25 20:46:09 -0400

    just breaking the story into components.

--- src/screens/LanguageApp/components/Lessons/Stories/stories.ts
+++ src/screens/LanguageApp/components/Lessons/Stories/stories.ts
@@ -31,28 +31,64 @@ export const stories: Story[] = [
         name: "second Lesson",
         sentences: [
             {
-                targetLanguage: `我的朋友
-                                我爸爸和妈妈是老师。我是学生。
-                                我有一个朋友。她是女人。她叫小月。她很高。
-                                今天，我喝水，她喝茶。我说：“你好吗？”
-                                她说：“我很好。你好吗？”
-                                我说：“我也很好。”
-                                她说：“再见！”
-                                我说：“再见！”
-                                我们都很高兴。`,
-                baseLanguage: `My Friend
-                            My father and mother are teachers. I am a student.
-                            I have a friend. She is a woman. Her name is Xiao Yue. She is very tall.
-                            Today, I drink water, she drinks tea. I say: "How are you?"
-                            She says: "I am very good. How are you?"
-                            I say: "I am also very good."
-                            She says: "Goodbye!"
-                            I say: "Goodbye!"
-                            We are all very happy.`,
+                targetLanguage: "我的朋友",
+                baseLanguage: "My Friend",
                 grammar: {
-                    highlight: "我是学生",
-                    note: "literal: I am some words or something"
+                    highlight: "todo el mundo",
+                    note: "literal: all the world"
                 }
+            },
+            {
+                targetLanguage: "我爸爸和妈妈是老师。我是学生。",
+                baseLanguage: "My father and mother are teachers. I am a student.",
+                grammar: {
+                    highlight: "todo el mundo",
+                    note: "literal: all the world"
+                }
+            },
+            {
+                targetLanguage: "我有一个朋友。她是女人。她叫小月。她很高。",
+                baseLanguage: "I have a friend. She is a woman. Her name is Xiao Yue. She is very tall.",
+                grammar: {
+                    highlight: "todo el mundo",
+                    note: "literal: all the world"
+                }
+            },
+            {
+                targetLanguage: "今天，我喝水，她喝茶。我说：“你好吗？”",
+                baseLanguage: "Today, I drink water, she drinks tea. I say: 'How are you?'",
+                grammar: {
+                    highlight: "todo el mundo",
+                    note: "literal: all the world"
+                }
+            },
+            {
+                targetLanguage: "她说：“我很好。你好吗？”",
+                baseLanguage: "She says: 'I am very good. How are you?'",
+                grammar: {
+                    highlight: "todo el mundo",
+                    note: "literal: all the world"
+                }
+            },
+            {
+                targetLanguage: "我说：“我也很好。",
+                baseLanguage: "I say: 'I am also very good.'",
+                grammar: null
+            },
+            {
+                targetLanguage: "她说：“再见”",
+                baseLanguage: " She says: 'Goodbye!'",
+                grammar: null
+            },
+            {
+                targetLanguage: "我说：“再见！”",
+                baseLanguage: "I say: 'Goodbye!'",
+                grammar: null
+            },
+            {
+                targetLanguage: "我们都很高兴。",
+                baseLanguage: "We are all very happy.",
+                grammar: null
             }
         ]
     }
