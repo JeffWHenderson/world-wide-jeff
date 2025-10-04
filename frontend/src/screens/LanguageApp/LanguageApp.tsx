ef8dae3515b8e4ed49a33362b198245f96964432
Jeff Henderson <jeffrey.henderson17@gmail.com>
2025-10-04 16:43:01 -0400

    Well this is a deployable state.. I think all the structure is there that I can finally make the backend for all of this.. I probably need to make READMEs and publish notes and get the course in a good state too... oh and CSS. OK this has a ton more work. I really should do that backend though.

--- frontend/src/screens/LanguageApp/LanguageApp.tsx
+++ frontend/src/screens/LanguageApp/LanguageApp.tsx
@@ -1,10 +1,20 @@
-// import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { lessons, Lesson } from "./components/Lessons/LessonList"
 import { LessonV1 } from "./LanguageAppTypes";
 import { courses } from "./courses/courses"; // TODO: obviously this will come from backend
 import { useEffect, useState } from "react";
 
+
+// TODO: add CSS Eventually
+const storyCardStyles = {
+    backgroundColor: 'red',
+    marginRight: "3px"
+}
+
+// TODO: add CSS Eventually
+const flashCardStyles = {
+    backgroundColor: 'orange',
+    marginRight: "3px"
+}
 
 const LanguageLearningApp = () => {
     const { language } = useParams();
@@ -12,16 +22,19 @@ const LanguageLearningApp = () => {
     const [course, setCourse] = useState(courses.Chinese);
 
     useEffect(() => {
-        if (language != "Chinese") { // TODO: obviously this will come from backend
+        if (language === "Spanish") { // TODO: obviously this will come from backend
             setCourse(courses.Spanish)
+        }
+        if (language === "Japanese") {
+            setCourse(courses.Japanese)
         } else {
             setCourse(courses.Chinese)
         }
     }, [language])
 
 
-    function handleSelectStory(lessonType: string, lesson?: Lesson | LessonV1) {
-        if (lessonType === 'flashcard') {
+    function handleSelectStory(lessonType: string, lesson?: LessonV1) {
+        if (lessonType === 'flashcard' || lessonType === 'flashcards') {
             navigator(`flashcards`, { state: { lesson: lesson, selectedLanguage: language } })
         } else {
             navigator(`story`, { state: { lesson: lesson, selectedLanguage: language } })
@@ -31,11 +44,11 @@ const LanguageLearningApp = () => {
     return (
         <>
             <h1>Leaning in {language}</h1>
-            <p>{course.section}</p>
+            <p>{course[0].section}</p>
             <div style={{ display: "flex", marginTop: '4px' }}>
                 {
-                    course.lessons.map(lesson => (
-                        <button style={{ backgroundColor: 'red', marginRight: "3px" }} onClick={() => handleSelectStory(lesson.type, lesson)} >{lesson.name}</button>
+                    course[0].lessons.map(lesson => (
+                        <button style={lesson.type === "story" ? storyCardStyles : flashCardStyles} onClick={() => handleSelectStory(lesson.type, lesson)} >{lesson.name}</button>
                     ))
                 }
             </div>
